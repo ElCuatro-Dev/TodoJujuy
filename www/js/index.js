19474ccb0a7992559da1540bd5e713f6174a3fe5
Marcelo <marcelo@users.noreply.github.com>
2015-04-29 10:26:29 -0300

    Update 1.4

--- www/js/index.js
+++ www/js/index.js
@@ -15,7 +15,8 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        //app.receivedEvent('deviceready');		
+        app.receivedEvent('deviceready');		
+		/*
 		var data = {							
 			"key": "test",
 			"noticias": true,
@@ -52,6 +53,7 @@ var app = {
 				console.log(responseData);
 			} 
 		});
+		*/
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
